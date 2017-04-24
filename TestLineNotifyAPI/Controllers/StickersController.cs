28715c3bbab5eb8341211802120bde6d47994de9
PoyChang <poypost@gmail.com>
2017-04-25 00:46:35 +0800

    增加取得貼圖檔的方法

--- TestLineNotifyAPI/Controllers/StickersController.cs
+++ TestLineNotifyAPI/Controllers/StickersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TestLineNotifyAPI.Models;
@@ -15,6 +16,20 @@ namespace TestLineNotifyAPI.Controllers
         /// <returns></returns>
         [HttpGet]
         public IActionResult GetStickers() => new JsonResult(FetchStickers());
+
+        // GET: api/Stickers/file
+        /// <summary>取得貼圖</summary>
+        /// <param name="stickerPackageId">貼圖包識別碼</param>
+        /// <param name="stickerId">貼圖識別碼</param>
+        /// <returns>貼圖</returns>
+        [HttpGet]
+        [Route("{stickerPackageId}/{stickerId}")]
+        public IActionResult GetSticker(string stickerPackageId, string stickerId)
+        {
+            var filename = FetchStickers().First(p => p.Stkpkgid == stickerPackageId && p.Stkid == stickerId).Img;
+            var image = System.IO.File.ReadAllBytes("Data/" + stickerPackageId + "/" + filename);
+            return new FileContentResult(image, "image/png");
+        }
 
         /// <summary>貼圖清單</summary>
         /// <returns></returns>
