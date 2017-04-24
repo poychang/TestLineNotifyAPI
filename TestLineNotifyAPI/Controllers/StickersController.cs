8550cec7f8d922e38d54e65e23f32e5d0ecfebbb
PoyChang <poypost@gmail.com>
2017-04-25 00:46:19 +0800

    加入貼圖控制器及模型，增加取得貼圖清單的方法

---
+++ TestLineNotifyAPI/Controllers/StickersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestLineNotifyAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestLineNotifyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class StickersController : Controller
+    {
+        // GET: api/Stickers
+        /// <summary>取得貼圖清單</summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetStickers() => new JsonResult(FetchStickers());
+
+        /// <summary>貼圖清單</summary>
+        /// <returns></returns>
+        private static IEnumerable<StickerModel> FetchStickers()
+        {
+            const string filePath = "Data/sticker_list.json";
+            var fileJson = System.IO.File.ReadAllText(filePath);
+
+            return JsonConvert.DeserializeObject<IEnumerable<StickerModel>>(fileJson);
+        }
+    }
+}
---
+++ TestLineNotifyAPI/Models/StickerModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TestLineNotifyAPI.Models
+{
+    /// <summary>貼圖</summary>
+    public class StickerModel
+    {
+        /// <summary>貼圖包識別碼</summary>
+        public string Stkpkgid { get; set; }
+        /// <summary>貼圖識別碼</summary>
+        public string Stkid { get; set; }
+        /// <summary>貼圖版本</summary>
+        public string Stkver { get; set; }
+        /// <summary>貼圖位置</summary>
+        public string Img { get; set; }
+    }
+}
